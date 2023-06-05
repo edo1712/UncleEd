0901395b088718e9680106221abca76b176b50c7
edo17 <edo1712@yandex.ru>
2023-06-05 22:36:33 +0300

    ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        float width = 128.1f;
-        float length = 256.1f;
-        boolean bool = length > width;
-        bool = false;
+        char ignore = 'f';
 
-        System.out.println(bool);
+
+        System.out.println(ignore);
 
 
 
