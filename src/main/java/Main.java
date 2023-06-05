e8027af4e101399124f12cb93de1e7a563538995
edo17 <edo1712@yandex.ru>
2023-06-05 22:17:35 +0300

    ...

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        float width = 128.1f;
+        float length = 256.1f;
+        boolean bool = length > width;
+        bool = (true);
+
+        System.out.println(bool);
+
+
+
+
+    }
+}
+
